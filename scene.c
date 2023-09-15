92d2d255bf27b85aa27a536c76cb829b85f11842
Rosa-Maria Makinen <rmakinen@c2r5p7.hive.fi>
2023-09-15 10:10:43 +0300

    added comments for my own tracking

--- scene.c
+++ scene.c
@@ -6,7 +6,7 @@
 /*   By: rmakinen <rmakinen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/13 09:44:07 by rmakinen          #+#    #+#             */
-/*   Updated: 2023/09/15 08:05:23 by rmakinen         ###   ########.fr       */
+/*   Updated: 2023/09/15 10:10:22 by rmakinen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,16 @@
 //first get ray_direction, store it in camera->norm_coord;
 //loop through the objects, for each object send to check hit, and object id
 //find the shortest distance and for that object fetch the information
-//for now, if it is a hit and the distance is the smallest, draw a colour on screen
+//for now, just if it is a hit and the distance is the smallest, draw a colour on screen
 
 void	get_distance(t_scene *img, t_camera *cam, t_hit *hit)
 {
-	//we loop check for the distance and update it to be the smallest distance
-	t_vec3	subtracted;
-	float	new_distance;
 	//distance is:
 	//d(P1,P2) = (x2-x1)2 + (y2-y1)2 + (z2-z1)2.
+
+	t_vec3	subtracted;
+	float	new_distance;
 	subtracted = vec_sub(cam->norm_coord, (*hit).pos);
-	//if not getting the hit being in front, add check here
 	new_distance = distance(subtracted);
 	if (new_distance < img->hit_data.distance)
 		img->hit_data.distance = new_distance;
@@ -42,7 +41,6 @@ int	get_closest_hit(t_camera *cam, t_scene *img, t_hit *hit, int x, int y)
 	//loop through the objects && get hit
 	//if hit, save the object and
 	t_scene		*temp;
-	//t_hit		hit;
 
 	temp = img;
 	img->hit_data.distance = FLT_MAX;
@@ -50,6 +48,7 @@ int	get_closest_hit(t_camera *cam, t_scene *img, t_hit *hit, int x, int y)
 	{
 		while (temp->objects->next != NULL)
 		{
+			//keep track of which object is which (maybe initialize the ids elsewhere)
 			//hit = get_hit(cam, temp->objects, x, y);
 			printf("type is %hd, hit is %i\n", temp->objects->type, hit->hit);
 			if ((*hit).hit == 1)
@@ -74,11 +73,12 @@ int	per_pixel(t_camera *cam, t_scene *img, int x, int y)
 	hit.hit = 1;
 
 	img->hit_data.distance = FLT_MAX;
-	get_ray_direction(cam, img, x, y); /// coordinate systems x & y
+	get_ray_direction(cam, img, x, y);
 	get_closest_hit(cam, img, &hit, x, y);
-	//
-	//
-	//
+	// func for normal
+	// func for color
+	// func for light
+	// etc
 
 
 	return (0);
